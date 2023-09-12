001296a443a8cdc310a4a9c8061d7f822bf0d697
Leny <leny.meziere@etu.unicaen.fr>
2023-09-12 10:03:19 +0200

    Possibilité d'arrêter timer et relancement)

--- js/script.js
+++ js/script.js
@@ -1,26 +1,46 @@
 let boutonTimer = document.getElementById("timer")
-let temps = 1500
+let temps = 5
 const timerElement = document.getElementById("divTimer")
 timerElement.innerText = parseInt(temps/60,10) + ":" + parseInt(temps%60,10)
 
 boutonTimer.addEventListener("click", () => {
     if(boutonTimer.textContent == "Lancer Timer") {
         boutonTimer.textContent = "Arrêter Timer"
-        setInterval(lancerTimer,1000)
-    }
+        let myInterval = setInterval(function() {
+            if(temps != 0) {
+                let minutes
+                let secondes
+                minutes = parseInt(temps/60,10)
+                secondes = parseInt(temps % 60,10)
+
+                temps = temps < 10 ? "o"+temps : temps
+
+                if(minutes < 10 && secondes < 10) {
+                    timerElement.innerText = "0" + minutes + ":" + "0" + secondes
+                }
+                if(minutes < 10 && secondes > 9) {
+                    timerElement.innerText = "0" + minutes + ":" + secondes
+                }
+                if(minutes > 9 && secondes < 10) {
+                    timerElement.innerText = minutes + ":" + "0" + secondes
+                }
+                else {
+                    timerElement.innerText = minutes + ":" + secondes
+                }
+                temps--
+            }
+            else {
+                temps = 300
+                myInterval
+            }
+            }, 1000)
+        }
     else {
         boutonTimer.textContent = "Lancer Timer"
-        stopTimer()
+        location.reload()
     }
 })
 
-lancerTimer = function() {
-    let minutes = parseInt(temps/60,10)
-    let secondes = parseInt(temps % 60,10)
-    timerElement.innerText = minutes + ":" + secondes
-    temps--
-}
-
-stopTimer = function() {
-    clearInterval(lancerTimer)
+function stopTimer() {
+    clearInterval(myInterval)
 }
