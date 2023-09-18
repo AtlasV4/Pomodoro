c0182bb682fe78175d8da97e28dd7b76a6006e84
Leny <leny.meziere@etu.unicaen.fr>
2023-09-18 11:09:16 +0200

    Timer presque fini + CSS

--- js/script.js
+++ js/script.js
@@ -1,46 +1,75 @@
 let boutonTimer = document.getElementById("timer")
-let temps = 5
-const timerElement = document.getElementById("divTimer")
-timerElement.innerText = parseInt(temps/60,10) + ":" + parseInt(temps%60,10)
+let tempsTravail
+let tempsPause
+let temps
+let compteur = 0
+let nbPomodori = document.getElementById("nbPomodori")
+let divTimer = document.getElementById("divTimer")
+let duree = document.getElementById("duree")
+
+let pause = document.getElementById("pause")
+let travail = document.getElementById("travail")
+travail.style.color = "yellow"
+let boutons = document.getElementById("boutons")
+
+let minutes
+let secondes
+
+getValue()
+console.log(tempsPause, tempsTravail)
 
 boutonTimer.addEventListener("click", () => {
-    if(boutonTimer.textContent == "Lancer Timer") {
-        boutonTimer.textContent = "Arrêter Timer"
-        let myInterval = setInterval(function() {
-            if(temps != 0) {
-                let minutes
-                let secondes
-                minutes = parseInt(temps/60,10)
-                secondes = parseInt(temps % 60,10)
-
-                temps = temps < 10 ? "o"+temps : temps
-
-                if(minutes < 10 && secondes < 10) {
-                    timerElement.innerText = "0" + minutes + ":" + "0" + secondes
-                }
-                if(minutes < 10 && secondes > 9) {
-                    timerElement.innerText = "0" + minutes + ":" + secondes
-                }
-                if(minutes > 9 && secondes < 10) {
-                    timerElement.innerText = minutes + ":" + "0" + secondes
-                }
-                else {
-                    timerElement.innerText = minutes + ":" + secondes
-                }
-                temps--
-            }
-            else {
-                temps = 300
-                myInterval
-            }
-            }, 1000)
-        }
+    if(boutonTimer.value == "Lancer Timer") {
+        boutonTimer.value = "Arrêter Timer"
+        setInterval(timer, 1000)
+    }
     else {
-        boutonTimer.textContent = "Lancer Timer"
+        boutonTimer.value = "Lancer Timer"
         location.reload()
     }
 })
 
-function stopTimer() {
-    clearInterval(myInterval)
+function timer() {
+    if(temps != 0) {
+        temps--
+        minutes = parseInt(temps/60,10) < 10 ? "0"+parseInt(temps/60,10) : parseInt(temps/60,10)
+        secondes = parseInt(temps % 60,10) < 10 ? "0"+parseInt(temps % 60,10) : parseInt(temps % 60,10)
+        duree.innerText = minutes + ":" + secondes
+    }
+    else {
+        compteur++
+        if (compteur % 2 === 0 && compteur % 8 !== 0) {
+            temps = tempsTravail * 60;
+            divTimer.style.backgroundColor = "#EA3712";
+            pause.style.color = "white";
+            travail.style.color = "yellow";
+            boutonTimer.style.backgroundColor = "#EA3712";
+            boutons.style.backgroundColor = "#EA3712";
+            nbPomodori.innerText = parseInt(nbPomodori.innerText) + 1
+        } else if (compteur % 8 === 0 && compteur % 2 === 0) {
+            nbPomodori.innerText = parseInt(nbPomodori.innerText) + 1
+            temps = 1200;
+            divTimer.style.backgroundColor = "green";
+            travail.style.color = "white";
+            pause.style.color = "yellow";
+            boutonTimer.style.backgroundColor = "green";
+            boutons.style.backgroundColor = "green";
+        } else if (compteur % 2 !== 0) {
+            temps = tempsPause * 60;
+            divTimer.style.backgroundColor = "green";
+            travail.style.color = "white";
+            pause.style.color = "yellow";
+            boutonTimer.style.backgroundColor = "green";
+            boutons.style.backgroundColor = "green";
+        }
+    }
+}
+
+function getValue() {
+    tempsTravail = document.getElementById("tempsTravail").value
+    tempsPause = document.getElementById("tempsPause").value
+    temps = tempsTravail * 60
+    minutes = parseInt(temps/60,10) < 10 ? "0"+parseInt(temps/60,10) : parseInt(temps/60,10)
+    secondes = parseInt(temps % 60,10) < 10 ? "0"+parseInt(temps % 60,10) : parseInt(temps % 60,10)
+    duree.innerText = minutes + ":" + secondes
 }
