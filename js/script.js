c2fdf620a404fa43a2bd4db17544a7eae2810196
AtlasV4 <lenymeziere@gmail.com>
2023-09-19 14:20:19 +0200

    Ajout Listener sur Input + localstorage

--- js/script.js
+++ js/script.js
@@ -1,46 +1,59 @@
-let boutonTimer = document.getElementById("timer")
-let tempsTravailInput = document.getElementById("tempsTravail")
-let tempsPauseInput = document.getElementById("tempsPause")
-let tempsLonguePauseInput = document.getElementById("tempsLonguePause")
-let temps
-let tempsLonguePause
-let tempsPause
-let tempsTravail
-let compteur = 0
-let nbPomodori = document.getElementById("nbPomodori")
-let divTimer = document.getElementById("divTimer")
-let duree = document.getElementById("duree")
+let boutonTimer = document.getElementById("timer");
+let tempsTravailInput = document.getElementById("tempsTravail");
+let tempsPauseInput = document.getElementById("tempsPause");
+let tempsLonguePauseInput = document.getElementById("tempsLonguePause");
+let temps;
+let tempsLonguePause;
+let tempsPause;
+let tempsTravail;
+let compteur = 0;
+let nbPomodori = document.getElementById("nbPomodori");
+let divTimer = document.getElementById("divTimer");
+let duree = document.getElementById("duree");
 
-let pause = document.getElementById("pause")
-let travail = document.getElementById("travail")
-travail.style.color = "yellow"
-let boutons = document.getElementById("boutons")
+let pause = document.getElementById("pause");
+let travail = document.getElementById("travail");
+travail.style.color = "yellow";
+let boutons = document.getElementById("boutons");
 
-let minutes
-let secondes
+let minutes;
+let secondes;
 
-const input = [tempsPauseInput,tempsLonguePauseInput,tempsTravailInput]
+const input = [tempsPauseInput, tempsLonguePauseInput, tempsTravailInput];
+
+tempsTravailInput.addEventListener("input", () => {
+    tempsTravail = parseInt(this.value, 10);
+    localStorage.setItem("tempsTravail", tempsTravail);
+});
+
+tempsPauseInput.addEventListener("input", () => {
+    tempsPause = parseInt(this.value, 10);
+    localStorage.setItem("tempsPause", tempsPause);
+});
+
+tempsLonguePauseInput.addEventListener("input", () => {
+    tempsLonguePause = parseInt(this.value, 10);
+    localStorage.setItem("tempsLonguePause", tempsLonguePause);
+});
 
 boutonTimer.addEventListener("click", () => {
-    if(boutonTimer.value == "Lancer Timer") {
-        boutonTimer.value = "Arrêter Timer"
-        setInterval(timer, 1000)
+    if (boutonTimer.value == "Lancer Timer") {
+        boutonTimer.value = "Arrêter Timer";
+        setInterval(timer, 1000);
+    } else {
+        boutonTimer.value = "Lancer Timer";
+        location.reload();
     }
-    else {
-        boutonTimer.value = "Lancer Timer"
-        location.reload()
-    }
-})
+});
 
 function timer() {
-    if(temps != 0) {
-        temps--
-        minutes = parseInt(temps/60,10) < 10 ? "0"+parseInt(temps/60,10) : parseInt(temps/60,10)
-        secondes = parseInt(temps % 60,10) < 10 ? "0"+parseInt(temps % 60,10) : parseInt(temps % 60,10)
-        duree.innerText = minutes + ":" + secondes
-    }
-    else {
-        compteur++
+    if (temps != 0) {
+        temps--;
+        minutes = parseInt(temps / 60, 10) < 10 ? "0" + parseInt(temps / 60, 10) : parseInt(temps / 60, 10);
+        secondes = parseInt(temps % 60, 10) < 10 ? "0" + parseInt(temps % 60, 10) : parseInt(temps % 60, 10);
+        duree.innerText = minutes + ":" + secondes;
+    } else {
+        compteur++;
         if (compteur % 2 === 0 && compteur % 9 !== 0) {
             temps = tempsTravail * 60;
             divTimer.style.backgroundColor = "#EA3712";
@@ -48,40 +61,56 @@ function timer() {
             travail.style.color = "yellow";
             boutonTimer.style.backgroundColor = "#EA3712";
             boutons.style.backgroundColor = "#EA3712";
-            nbPomodori.innerText = parseInt(nbPomodori.innerText) + 1
+            nbPomodori.innerText = parseInt(nbPomodori.innerText) + 1;
         } else if (compteur % 9 === 0) {
             temps = tempsLonguePause * 60;
-            couleurPause()
+            couleurPause();
         } else if (compteur % 2 !== 0) {
             temps = tempsPause * 60;
-            couleurPause()
+            couleurPause();
         }
     }
 }
 
-function getValue() {
-    tempsTravail = tempsTravailInput.value
-    tempsPause = tempsPauseInput.value
-    tempsLonguePause = tempsLonguePauseInput.value
-    temps = tempsTravail * 60
-    minutes = parseInt(temps/60,10) < 10 ? "0"+parseInt(temps/60,10) : parseInt(temps/60,10)
-    secondes = parseInt(temps % 60,10) < 10 ? "0"+parseInt(temps % 60,10) : parseInt(temps % 60,10)
-    duree.innerText = minutes + ":" + secondes
-}
+document.addEventListener("DOMContentLoaded", () => {
+    tempsTravail = parseInt(localStorage.getItem("tempsTravail") || 25, 10);
+    tempsPause = parseInt(localStorage.getItem("tempsPause") || 5, 10);
+    tempsLonguePause = parseInt(localStorage.getItem("tempsLonguePause") || 20, 10);
+
+    tempsTravailInput.value = tempsTravail;
+    tempsPauseInput.value = tempsPause;
+    tempsLonguePauseInput.value = tempsLonguePause;
+
+    temps = tempsTravail * 60;
+    minutes = parseInt(temps / 60, 10) < 10 ? "0" + parseInt(temps / 60, 10) : parseInt(temps / 60, 10);
+    secondes = parseInt(temps % 60, 10) < 10 ? "0" + parseInt(temps % 60, 10) : parseInt(temps % 60, 10);
+    duree.innerText = minutes + ":" + secondes;
+});
 
 function couleurPause() {
     divTimer.style.backgroundColor = "green";
-            travail.style.color = "white";
-            pause.style.color = "yellow";
-            boutonTimer.style.backgroundColor = "green";
-            boutons.style.backgroundColor = "green";
+    travail.style.color = "white";
+    pause.style.color = "yellow";
+    boutonTimer.style.backgroundColor = "green";
+    boutons.style.backgroundColor = "green";
 }
 
-
-for(let resultat of input) {
-    resultat.addEventListener('input', function(){
+for (let resultat of input) {
+    resultat.addEventListener("input", () => {
         let valeur = this.value.match(/^\d+$/);
         if (valeur === null) {
             this.value = "";
         }
-})}
+    });
+}
+
+function recupValeurs() {
+    tempsTravail = parseInt(tempsTravailInput.value);
+    tempsPause = parseInt(tempsPauseInput.value);
+    tempsLonguePause = parseInt(tempsLonguePauseInput.value);
+    temps = tempsTravail * 60;
+    minutes = parseInt(temps / 60, 10) < 10 ? "0" + parseInt(temps / 60, 10) : parseInt(temps / 60, 10);
+    secondes = parseInt(temps % 60, 10) < 10 ? "0" + parseInt(temps % 60, 10) : parseInt(temps % 60, 10);
+    duree.innerText = minutes + ":" + secondes;
+}
+
