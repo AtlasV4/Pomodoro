80d12e46d91e069541b8ef6172505bc2a33e0466
AtlasV4 <lenymeziere@gmail.com>
2023-09-19 14:39:40 +0200

    Ajout des commentaires

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Sélection des éléments HTML dont on a besoin
 let boutonTimer = document.getElementById("timer");
 let tempsTravailInput = document.getElementById("tempsTravail");
 let tempsPauseInput = document.getElementById("tempsPause");
@@ -21,31 +22,36 @@ let secondes;
 
 const input = [tempsPauseInput, tempsLonguePauseInput, tempsTravailInput];
 
+// Gestionnaire d'événements pour le champ "tempsTravailInput"
 tempsTravailInput.addEventListener("input", (e) => {
-    tempsTravail = parseInt(e.target.value, 10);
-    localStorage.setItem("tempsTravail", tempsTravail);
+    tempsTravail = parseInt(e.target.value, 10); // Convertit la valeur en nombre
+    localStorage.setItem("tempsTravail", tempsTravail); // Stocke la valeur dans le localStorage
 });
 
+// Gestionnaire d'événements pour le champ "tempsPauseInput"
 tempsPauseInput.addEventListener("input", (e) => {
-    tempsPause = parseInt(e.target.value, 10);
-    localStorage.setItem("tempsPause", tempsPause);
+    tempsPause = parseInt(e.target.value, 10); // Convertit la valeur en nombre
+    localStorage.setItem("tempsPause", tempsPause); // Stocke la valeur dans le localStorage
 });
 
+// Gestionnaire d'événements pour le champ "tempsLonguePauseInput"
 tempsLonguePauseInput.addEventListener("input", (e) => {
-    tempsLonguePause = parseInt(e.target.value, 10);
-    localStorage.setItem("tempsLonguePause", tempsLonguePause);
+    tempsLonguePause = parseInt(e.target.value, 10); // Convertit la valeur en nombre
+    localStorage.setItem("tempsLonguePause", tempsLonguePause); // Stocke la valeur dans le localStorage
 });
 
+// Gestionnaire d'événements pour le bouton "boutonTimer"
 boutonTimer.addEventListener("click", () => {
     if (boutonTimer.value == "Lancer Timer") {
         boutonTimer.value = "Arrêter Timer";
-        setInterval(timer, 1000);
+        setInterval(timer, 1000); // Lance le timer avec une intervalle de 1 seconde
     } else {
         boutonTimer.value = "Lancer Timer";
-        location.reload();
+        location.reload(); // Recharge la page pour réinitialiser le timer
     }
 });
 
+// Fonction de mise à jour du minuteur / du minuteur
 function timer() {
     if (temps != 0) {
         temps--;
@@ -72,10 +78,11 @@ function timer() {
     }
 }
 
+// Gestionnaire d'événements lorsque le DOM est complètement chargé
 document.addEventListener("DOMContentLoaded", () => {
-    tempsTravail = parseInt(localStorage.getItem("tempsTravail") || 25, 10);
-    tempsPause = parseInt(localStorage.getItem("tempsPause") || 5, 10);
-    tempsLonguePause = parseInt(localStorage.getItem("tempsLonguePause") || 20, 10);
+    tempsTravail = parseInt(localStorage.getItem("tempsTravail") || 25, 10); // Récupère la valeur depuis le localStorage
+    tempsPause = parseInt(localStorage.getItem("tempsPause") || 5, 10); // Récupère la valeur depuis le localStorage
+    tempsLonguePause = parseInt(localStorage.getItem("tempsLonguePause") || 20, 10); // Récupère la valeur depuis le localStorage
 
     temps = tempsTravail * 60;
 
@@ -88,7 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
     duree.innerText = minutes + ":" + secondes;
 });
 
-
+// Fonction pour changer les couleurs pendant les pauses
 function couleurPause() {
     divTimer.style.backgroundColor = "green";
     travail.style.color = "white";
@@ -97,6 +104,7 @@ function couleurPause() {
     boutons.style.backgroundColor = "green";
 }
 
+// Gestionnaires d'événements pour les champs de saisie pour assurer la saisie de nombres
 for (let resultat of input) {
     resultat.addEventListener("input", (e) => {
         let valeur = e.target.value.match(/^\d+$/);
@@ -106,6 +114,7 @@ for (let resultat of input) {
     });
 }
 
+// Fonction pour récupérer les valeurs des champs de saisie et mettre à jour le minuteur
 function recupValeurs() {
     tempsTravail = parseInt(tempsTravailInput.value);
     tempsPause = parseInt(tempsPauseInput.value);
